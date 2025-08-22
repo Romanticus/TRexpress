d2f6c7bc22c36892d959745081a204187bba7d68
Roma Bludilin <romabludilin@yandex.ru>
2025-08-22 18:19:37 +0400

    feat: добавлена проверка авторизации

---
+++ src/middlewares/accessMiddleware.ts
@@ -0,0 +1,42 @@
+import { Request, Response,NextFunction } from "express";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+// import { JwtPayload } from "../utils/types";
+import { HttpStatus } from "../utils/constants";
+
+interface SessionRequest extends Request {
+    user?: string | JwtPayload;
+}
+
+
+const handleAuthError = (res: Response) => {
+  res
+  .status(HttpStatus.UNAUTHORIZED)
+  .send({ message: 'Необходима авторизация' });
+};
+
+const extractBearerToken = (header: string) => {
+  return header.replace('Bearer ', '');
+};
+
+export default function accessMiddleware(
+  req: SessionRequest,
+  res: Response,
+  next: NextFunction,
+) {
+    const { authorization }= req.headers;
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+    return handleAuthError(res);
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
+  try {
+    payload = jwt.verify(token, 'super-strong-secret');
+  } catch (err) {
+    return handleAuthError(res);
+  }
+  req.user= payload;
+
+  next();
+}
