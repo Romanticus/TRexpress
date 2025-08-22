501745ff1730c14e2e54a8a9764003f44f7e9f11
Roma Bludilin <romabludilin@yandex.ru>
2025-08-22 20:52:19 +0400

    fix: название енв переменных

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,9 +3,9 @@ import "dotenv/config";
 export const PORT = Number(process.env.PORT) || 3000;
 
 export const JWT_ACCESS_EXPIRES_IN =
-  (process.env.AUTH_ACCESS_TOKEN_EXPIRY as any) || "15m";
+  (process.env.JWT_ACCESS_EXPIRES_IN as any) || "15m";
 export const JWT_REFRESH_EXPIRES_IN =
-  (process.env.AUTH_REFRESH_TOKEN_EXPIRY as any) || "7d";
+  (process.env.JWT_REFRESH_EXPIRES_IN as any) || "7d";
 export const JWT_ACCESS_SECRET = process.env.JWT_SECRET || "SecretAccessToken";
 export const JWT_REFRESH_SECRET =
   process.env.JWT_REFRESH_SECRET || "SecretRefreshToken";
